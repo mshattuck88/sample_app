763be48c89832bd72dd4b8695365d895ef6bc3a8
Morgan Shattuck <mshattuck88@gmail.com>
2016-10-05 16:46:42 -0400

    Created an about page and added bootstrap

--- web.py
+++ web.py
@@ -5,6 +5,10 @@ app = Flask(__name__) # special python variable
 def index(): # must be right undernear the above line
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 app.run(debug=True) # run function comes as a part of Flask
 
 # type in localhost:5000 in browser to see your webpage
